d4865961c3a6fe6a422b80a50b9c87b9e8fcd84a
Lucien Heart <88631572+lucien-heart@users.noreply.github.com>
2023-06-13 21:59:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember()
+{
+    var member = new Object();
+    member.name = "John";
+    member.age = 25;
+    member.skills = ["C++", "Java", "Python"];
+    member.address = {
+        street: "123 Main St",
+        city: "New York",
+        state: "NY"
+    };
+    return member;
+}   
